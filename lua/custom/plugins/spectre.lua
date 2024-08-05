73adc41efad0611c775c061a014426365e2cdecc
Tomas Sladecek <tomas.sladecek@geneton.sk>
2024-08-05 13:44:06 +0200

    spectre replace

--- lua/custom/plugins/spectre.lua
+++ lua/custom/plugins/spectre.lua
@@ -4,6 +4,7 @@ return {
     'folke/trouble.nvim',
   },
   init = function()
+    local s = require 'spectre'
     vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
       desc = 'Toggle Spectre',
     })
@@ -15,6 +16,12 @@ return {
     })
     vim.keymap.set('n', '<leader>Sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
       desc = 'Search on current file',
+    })
+    vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+      desc = 'Toggle Spectre',
+    })
+    vim.keymap.set('n', '<leader>Sr', "<cmd>lua require('spectre.actions').run_replace()<CR>", {
+      desc = '[r]eplace',
     })
   end,
 }
